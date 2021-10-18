443c1c8ac1c66af61114e6c19edbe66a7109fab9
rojo5 <luis21197@gmail.com>
2021-10-18 20:08:09 +0200

    Creado menu a partir de un JSON

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_componentes/src/providers/menu_provider.dart';
+import 'package:flutter_componentes/src/utils/icono_string_util.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,18 +16,39 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        print('snapshot:');
+        print(snapshot.data);
 
-   print( menuProvider.opciones);
-
-
-    return ListView(
-      children: _listaItems(),
+        return ListView(
+          children: _listaItems(snapshot.data ?? []),
+        );
+      },
     );
   }
 
-  List<Widget> _listaItems() {
-    return [
-      ListTile()
-    ]; 
+  List<Widget> _listaItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: getIcon(opt['icon']),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {
+          // ignore: todo
+          // TODO
+        },
+      );
+
+      opciones
+        ..add(widgetTemp)
+        ..add(Divider());
+    });
+
+    return opciones;
   }
 }
