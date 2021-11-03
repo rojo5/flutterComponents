a1401f043eaac4e992342376045fe39d60e1e483
rojo5 <luis21197@gmail.com>
2021-11-03 20:40:23 +0100

    Alertas

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -10,6 +10,15 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar Alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape:  StadiumBorder(),
+          onPressed: () => _mostrarAlert(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.folder_open_sharp),
         onPressed: () {
@@ -17,5 +26,30 @@ class AlertPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void _mostrarAlert(BuildContext context) {
+    showDialog(
+      context: context, 
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es el contenido de la alerta'),
+              FlutterLogo(size: 100.0,)
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(onPressed: () => Navigator.of(context).pop(), child: Text('Cancelar')),
+            TextButton(onPressed: () {}, child: Text('Ok')),
+          ],
+        );
+      }
+      );
+       
   }
 }
