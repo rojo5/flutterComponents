fe85229c5fb08383e1148d98cf253d7eb369cd61
rojo5 <luis21197@gmail.com>
2021-12-04 13:11:07 +0100

    AnimatedContainer

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_componentes/src/pages/alert_page.dart';
+import 'package:flutter_componentes/src/pages/animated_container.dart';
 import 'package:flutter_componentes/src/pages/avatar_page.dart';
 import 'package:flutter_componentes/src/pages/card_page.dart';
 import 'package:flutter_componentes/src/pages/home_page.dart';
@@ -9,6 +10,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     '/': (BuildContext context) => HomePage(),
     'alert': (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
-    'card' : (BuildContext context) => CardPage()
+    'card' : (BuildContext context) => CardPage(),
+    'animatedContainer' : (BuildContext context) => AnimatedContainerPage()
   };
 }
